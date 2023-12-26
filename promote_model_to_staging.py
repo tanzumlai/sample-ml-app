4e26a87f593b2e91698b924ec8f8a41e193b619b
Tola Awofolu <oawofolu@pivotal.io>
2023-12-26 10:58:01 -0500

    Updated formatting

--- promote_model_to_staging.py
+++ promote_model_to_staging.py
@@ -1,5 +1,4 @@
 from app.analytics import cifar_cnn
 import sys
 
-cifar_cnn.promote_model_to_staging(base_model_name=sys.argv[1], candidate_model_name=sys.argv[2], evaluation_dataset_name=sys.argv[3],
-                                   model_flavor=sys.argv[4])
+cifar_cnn.promote_model_to_staging(base_model_name=sys.argv[1], candidate_model_name=sys.argv[2], evaluation_dataset_name=sys.argv[3], model_flavor=sys.argv[4])
