a3b0ec182a7eee87ff129073f27ae33eeece23a7
Tola Awofolu <oawofolu@pivotal.io>
2023-12-28 02:35:05 -0500

    Syntax updates/data type casting

--- train_model.py
+++ train_model.py
@@ -4,4 +4,4 @@ import sys
 
 dataset_path = cifar_cnn.download_dataset(artifact=sys.argv[1])
 data = hkl.load(dataset_path)
-cifar_cnn.train_model(model_name=sys.argv[2], model_flavor=sys.argv[3], model_stage=sys.argv[4], data=data, epochs=sys.argv[5])
+cifar_cnn.train_model(model_name=sys.argv[2], model_flavor=sys.argv[3], model_stage=sys.argv[4], data=data, epochs=int(sys.argv[5]))
